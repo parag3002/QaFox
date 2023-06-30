3af1b03d0f88f188414c8ae04b7171c73c41650d
parag3002 <debu05101987@gmail.com>
2023-06-30 23:49:02 +0530

    June_30_Last

--- QAFox/src/test/java/base/baseClass.java
+++ QAFox/src/test/java/base/baseClass.java
@@ -1,29 +1,42 @@
 package base;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class baseClass 
 {
 
+	public FileInputStream FIS;
+	public Properties PS;
+	public WebDriver D;
 	
-	baseClass()
+	
+	public baseClass() throws Exception
 	{
-		
+		D=new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
 		
 	}
 	
 	@BeforeMethod
-	public void setup()
+	public void setup() throws Exception
 	{
-		System.out.println("setUp Drivers In Progress...");
+		//baseClass B = new baseClass();
 	}
 	
 	
 	@AfterMethod
 	public void tearDown()
 	{
-		System.out.println("TearDown Complete.../");
+		D.close();
+		D.quit();
 	}
 }
--- QAFox/src/test/java/testCase/TC1_SignUp.java
+++ QAFox/src/test/java/testCase/TC1_SignUp.java
@@ -1,21 +1,26 @@
 package testCase;
 
 import org.testng.annotations.Test;
+import base.*;
 
-public class TC1_SignUp 
+public class TC1_SignUp extends baseClass
 {
 
-	TC1_SignUp()
+	TC1_SignUp() throws Exception
 	{
 		
 	
 	}
 	
 	@Test
-	public void TC1_Method()
+	public void TC1_Method() throws Exception
 	{
 		System.out.println("TC 1 Method...");
 		System.out.println("TC 1 Method...Again");
+		D.get("https://www.facebook.com");
+		D.manage().window().maximize();
+		Thread.sleep(4000);
+		
 	}
 	
 	
